2e5d591b366ab3f7e46e79db8c925b581ccb4913
Watcharapong Suksrisawangwong <pong8aor@gmail.com>
2024-03-31 13:38:36 +0700

    Add script for inserting data

---
+++ kbazaar/src/main/resources/sql/data/cartItem.sql
@@ -0,0 +1,6 @@
+INSERT INTO cart_item ("id", "username", "sku", "name", "price", "quantity", "discount", "promotion_codes") VALUES
+(1, 'TechNinja', 'asda', 'a', 2.00, 1.00, 1.00, 'asdas');
+INSERT INTO cart_item ("id", "username", "sku", "name", "price", "quantity", "discount", "promotion_codes") VALUES
+(2, 'CodeMaster', 'asda', 'a', 2.00, 1.00, 1.00, 'asdas');
+INSERT INTO cart_item ("id", "username", "sku", "name", "price", "quantity", "discount", "promotion_codes") VALUES
+(3, 'CodeMaster', 'asdaa', 'a', 2.00, 1.00, 1.00, 'asdas');
